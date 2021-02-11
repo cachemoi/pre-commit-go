f31f3d6e54f32e139730657365aefeb1e62d7fca
cachemoi <m.rothierbautzer@gmail.com>
2021-02-11 17:54:53 +0000

    fix goimports

--- run-go-imports.sh
+++ run-go-imports.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
-
-set -euo pipefail
-
-# THanks to Kshitiz Sharma
 #
-# https://stackoverflow.com/questions/3578584/bash-how-to-delete-elements-from-an-array-based-on-a-pattern
-filter_arr() {
-    arr=("$@")
-    arr=(${arr[@]:1})
-    dirs=($(for i in "${arr[@]}"
-        do echo $i
-    done | grep -v "$1"))
-    echo "${dirs[@]}"
-}
+# Capture and print stdout, since goimports doesn't use proper exit codes
+#
+set -e
 
-goimports -l -w "$(filter_arr "vendor/" "$@")"
+exec 5>&1
+output="$(goimports -l -w "$@" | tee /dev/fd/5)"
+[[ -z "$output" ]]
